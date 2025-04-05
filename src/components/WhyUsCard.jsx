635e496ea138ea990785f55d72a6456509834a77
Bina <benjaminbinyam500@gmail.com>
2025-04-05 15:12:22 +0300

    feat: create reusable WhyUsCard component with icon and CTA
    - Added `WhyUsCard` component for displaying feature highlights.
    - Includes icon, title, description, and "Learn more" button with arrow icon.
    - Utilizes `react-icons` for visual enhancement.

---
+++ src/components/WhyUsCard.jsx
@@ -0,0 +1,23 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const WhyUsCard = ({ icon, cardTitle, text }) => {
+  return (
+    <div className="card card-border bg-base-100 w-[330px] shadow-2xl">
+      <div className="card-body space-y-4">
+        <img src={icon} alt="best-service" className="w-16 h-16" />
+        <h2 className="card-title font-bold">{cardTitle}</h2>
+        <p className="text-xl text-gray-500">{text}</p>
+        <div className="card-actions justify-start">
+          <button className="btn text-black">
+            Learn more
+            <span>
+              <FaArrowRightLong className="inline" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyUsCard;
