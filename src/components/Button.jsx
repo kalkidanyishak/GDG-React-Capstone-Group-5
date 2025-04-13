4d36b9b84c121aed2bc23a2a9c40d1b75228bbe0
Bina <benjaminbinyam500@gmail.com>
2025-04-13 14:29:52 +0300

    fix: fix bug on the Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
-const Button = ({ children, onClick, className = "" }) => {
+const Button = ({ type = "button", children, onClick, className = "" }) => {
   return (
     <button
+      type={type}
       onClick={onClick}
-      className={`px-4 py-2 font-semibold text-white bg-black rounded-full duration-200 hover:bg-black/80 hover:cursor-pointer focus:outline-none ${className}`}
+      className={`text-[10px] md:text-sm px-2 md:px-4 py-3 font-semibold text-white bg-black rounded-full duration-200 hover:bg-black/80 hover:cursor-pointer focus:outline-none ${className}`}
     >
       {children}
     </button>
   );
 };
+
 export default Button;
